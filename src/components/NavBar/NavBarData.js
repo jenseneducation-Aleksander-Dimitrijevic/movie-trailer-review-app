7bc6b743d11a4e2e3be17d9822d08ec97b3d17bd
unknown <elam@nabi.nu>
2021-04-15 19:35:21 +0200

    Navbar + routes fixed

---
+++ src/components/NavBar/NavBarData.js
@@ -0,0 +1,28 @@
+export const NavBarData = [
+    {
+      title: "Home",
+      path: "/",
+      id: 1
+    },
+    {
+      title: "Movies",
+      path: "/movies",
+      id: 2
+    },
+    {
+      title: "Series",
+      path: "/series",
+      id: 3
+    },
+    {
+      title: "Kids",
+      path: "/kids",
+      id: 4
+    },
+    {
+      title: "Documentary",
+      path: "/documentary",
+      id: 5
+    }
+  ];
+  
