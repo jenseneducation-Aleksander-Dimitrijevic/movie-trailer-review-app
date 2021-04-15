cc70fb05a45883714ceb68e3c8e6a917aff3b3e3
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2021-04-15 18:06:57 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home } from "./views/Home/Home";
+
+function App() {
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
+  );
+}
+
+export default App;
