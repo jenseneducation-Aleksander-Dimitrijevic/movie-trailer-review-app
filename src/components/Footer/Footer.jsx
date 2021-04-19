be6c8f143ea6c180f8b3ed4521033fc1b8a4618d
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2021-04-19 17:54:42 +0200

    start styling footer - work in progress

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,17 +6,22 @@ export const Footer = () => {
   return (
     <FooterContainer>
       <header className="header">
-        <h1>This is footer</h1>
-        <h2>
+        <h1 className="title">Om Watchy</h1>
+        <h2 className="sub-title">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
           excepturi odio sapiente labore nobis fugiat, itaque sunt perspiciatis
           voluptatibus totam.
         </h2>
       </header>
       {NavBarData.map((n) => (
-        <ul key={n.id}>
-          <li>
-            <NavLink exact to={n.path} activeClassName="active">
+        <ul key={n.id} className="list">
+          <li className="list-item">
+            <NavLink
+              exact
+              to={n.path}
+              activeClassName="active"
+              className="list-link"
+            >
               <span>{n.title}</span>
             </NavLink>
           </li>
--- src/components/Footer/FooterStyled.js
+++ src/components/Footer/FooterStyled.js
@@ -1,4 +1,24 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
-export const FooterContainer = styled.footer``;
+import { ColorsGlobal as color } from "../../styles/Colors";
+export const FooterContainer = styled.footer`
+  background: ${color.darkBlue};
+  padding: 2rem;
+
+  h1.title {
+    color: ${color.white};
+    text-transform: uppercase;
+    font-size: 18px;
+    letter-spacing: 0.05em;
+    padding: 0 0 20px 0;
+  }
+
+  h2.sub-title {
+    font-size: 16px;
+    line-height: 1.5;
+    font-weight: 400;
+    letter-spacing: 0.03em;
+    color: ${color.white};
+  }
+`;
 export const NavLink = styled(Link)``;
