27463d606b807b9412fce9cb55b13e8498e7aac8
unknown <elam@nabi.nu>
2021-04-24 17:27:27 +0200

    changed font

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,13 +2,12 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
 
 * {
-    @import url('https://fonts.googleapis.com/css2?family=Andika+New+Basic&display=swap');
     padding:0;
     margin:0;
     box-sizing:border-box;
-    font-family: 'Andika New Basic', sans-serif;
-
     
 }
-
+body {
+    font-family: 'Montserrat', sans-serif;
+}
 `;
