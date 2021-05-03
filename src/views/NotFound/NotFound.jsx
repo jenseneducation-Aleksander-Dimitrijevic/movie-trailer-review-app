7976eb43f97818b5ce4c95b6aa4a33bfe2606ca6
max  aakre <maxaakre@MacBook-Air-som-tillhor-max.local>
2021-05-03 20:47:37 +0200

    startded with 404 text

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {NotFoundContainer} from './NotFound.styles'
 
-export const NotFound = () => {
+export const NotFound = () => {  
   return (
-    <div>
+    <NotFoundContainer>
       <h1>404 Not found</h1>
-    </div>
+    </NotFoundContainer>
   );
 };
---
+++ src/views/NotFound/NotFound.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const NotFoundContainer = styled.div`
+padding-top: 10em;
+width 100%;
+text-align:center;
+font-size:42px;
+
+`
