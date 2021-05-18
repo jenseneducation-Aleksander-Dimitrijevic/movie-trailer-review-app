8fff1c952a6915deaeedf3e86c7df7cd297f3fbd
Max Aakre <maxaakre@gmail.com>
2021-05-18 19:02:08 +0200

    Added FAW page

---
+++ src/views/Faq/FaqComponent.jsx
@@ -0,0 +1,19 @@
+
+
+export default function  FaqQuestions  ({faq, index, toggleFAQ})  {
+    return(
+        <div
+			className={"faq " + (faq.open ? 'open' : '')}
+			key={index}
+			onClick={() => toggleFAQ(index)    
+            }
+		>
+			<div className="faq-question">
+				{faq.question}
+			</div>
+			<div className="faq-answer">
+				{faq.answer}
+			</div>
+		</div>
+    )
+}
