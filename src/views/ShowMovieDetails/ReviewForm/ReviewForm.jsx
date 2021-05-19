3d6ced7c20235e56a6c1917c67bd4c7f225253a0
Aleksander Dimitrijevic <54711789+jenseneducation-Aleksander-Dimitrijevic@users.noreply.github.com>
2021-05-19 21:40:10 +0200

    feature/show movie info page (#49)
    * attached responsive styles
    
    * modified styles for show movie view, added media query function in new folder called helpers
    
    * modified styles on backdrop image
    
    * validate user input and check if user is logged in on review form
    
    * show movie view mvp, done + fix bug on about-page route

--- src/views/ShowMovieDetails/ReviewForm/ReviewForm.jsx
+++ src/views/ShowMovieDetails/ReviewForm/ReviewForm.jsx
@@ -3,16 +3,27 @@ import ReviewFormContainer from "./ReviewFormStyles";
 
 export default function ReviewForm() {
   const [review, setReview] = useState("");
-  const handleReview = (e) => setReview(e.target.value);
+  const handleReview = () => {
+    if (!sessionStorage.getItem("__user__")) {
+      alert("You got to sign in to post a review.");
+      return;
+    }
+  };
+  const handleReviewSubmit = (e) => {
+    e.preventDefault();
+    if (!review) return;
+    console.log("submitted");
+  };
   return (
-    <ReviewFormContainer>
+    <ReviewFormContainer onSubmit={handleReviewSubmit}>
       <h1 className="review-form-title">Comments</h1>
       <input
         type="text"
         className="review-form-input"
         placeholder="Comment/tag a friend"
         value={review}
-        onChange={handleReview}
+        onClick={handleReview}
+        onChange={(e) => setReview(e.target.value)}
       />
     </ReviewFormContainer>
   );
--- src/views/ShowMovieDetails/ShowMovieStyles.js
+++ src/views/ShowMovieDetails/ShowMovieStyles.js
@@ -1,64 +1,119 @@
 import styled from "styled-components";
 import { ColorsGlobal as color } from "../../styles/Colors";
+
 export default styled.div`
   padding: 110.5px 0 0;
 
-  .backdrop-container {
-    box-shadow: inset 0px -39px 36px -21px ${color.deepBlue};
-  }
-
-  .backdrop-image {
-    display: block;
+  .show-movie {
     width: 100%;
-    filter: grayscale(15%);
-    opacity: 0.4;
-    z-index: -1;
+    height: 300px;
+    background: no-repeat top center/cover;
+    box-shadow: inset 0px -250px 250px -10px rgba(20, 33, 44, 0.99);
     position: relative;
-  }
+    display: flex;
 
-  .movie-details {
-    padding: 0 1rem;
-    margin: -3rem 0 0;
-    &-rating {
-      display: flex;
-      align-items: center;
-      padding: 5px 0 0;
+    @media screen and (min-width: 768px) {
+      height: 500px;
+      box-shadow: inset 0px -350px 200px -10px rgba(20, 33, 44, 0.99);
+    }
 
-      &-star {
-        color: yellow;
-        margin: 0 5px 0 0;
+    @media screen and (min-width: 1440px) {
+      height: 600px;
+    }
+
+    &::before {
+      width: 100%;
+      height: 100%;
+      background: rgba(20, 33, 44, 0.3);
+      content: "";
+      top: 0;
+      left: 0;
+      position: absolute;
+    }
+
+    &-backdrop {
+      display: none;
+      height: 350px;
+      flex: 0 0 220px;
+      border-radius: 5px;
+      background: no-repeat center center/cover;
+      box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.1);
+
+      @media screen and (min-width: 768px) {
+        display: block;
+      }
+    }
+
+    &-content {
+      align-self: flex-end;
+      margin: -8rem 0;
+      width: 100%;
+      position: relative;
+      color: ${color.white};
+
+      .movie-details {
+        align-self: flex-end;
       }
 
-      &-number {
-        color: ${color.lightBlue};
-        font-size: 14px;
-        font-weight: 600;
+      @media screen and (min-width: 768px) {
+        display: flex;
+        margin: auto;
+        padding: 0 2rem;
       }
-    }
-    &-title {
-      font-size: 20px;
-      text-transform: uppercase;
-      font-weight: 800;
-      color: ${color.white};
-      letter-spacing: 0.03em;
-      margin: 8px 0;
-    }
 
-    &-release {
-      font-weight: 600;
-      font-size: 14px;
-      letter-spacing: 0.03em;
-      color: ${color.white};
-      margin: 6px 0;
-      display: block;
-    }
+      .movie-details {
+        padding: 0 1rem;
 
-    &-overview {
-      color: ${color.lightBlue};
-      letter-spacing: 0.03em;
-      line-height: 1.7;
-      font-weight: 600;
-      font-size: 14px;
+        @media screen and (min-width: 768px) {
+          padding: 0 2rem;
+        }
+
+        &-rating {
+          display: flex;
+
+          &-star {
+            color: yellow;
+            margin: 0 5px 0 0;
+          }
+
+          &-number {
+            color: ${color.lightBlue};
+            font-size: 12px;
+            font-weight: 600;
+            align-self: flex-end;
+          }
+        }
+        &-title {
+          font-size: 20px;
+          text-transform: uppercase;
+          font-weight: 800;
+          color: ${color.white};
+          letter-spacing: 0.03em;
+          margin: 8px 0;
+
+          @media screen and (min-width: 768px) {
+            font-size: 2.2rem;
+            letter-spacing: 0.05em;
+          }
+        }
+
+        &-release {
+          font-weight: 600;
+          font-size: 14px;
+          letter-spacing: 0.03em;
+          color: ${color.white};
+          margin: 6px 0;
+          display: block;
+        }
+
+        &-overview {
+          color: ${color.lightBlue};
+          letter-spacing: 0.03em;
+          line-height: 1.7;
+          font-weight: 600;
+          font-size: 14px;
+        }
+      }
     }
   }
 `;
