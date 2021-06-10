206b3ef23833dbf2df06ab49bb5049facd04edd2
Aleksander Dimitrijevic <54711789+jenseneducation-Aleksander-Dimitrijevic@users.noreply.github.com>
2021-06-10 21:43:28 +0200

    feature/auth user (#62)
    * attached responsive styles
    
    * modified styles for show movie view, added media query function in new folder called helpers
    
    * modified styles on backdrop image
    
    * validate user input and check if user is logged in on review form
    
    * setup mongoose as database
    
    * signup user to database
    
    * fix login route with authentication
    
    * authenticate user upon login and signup
    
    * setup review form
    
    * render signup/login form if user not logged in, working on review form
    
    * user can now submit reviews
    
    * added new property to reviews model
    
    * properly render reviews, fix minor bugs
    
    * working on reviews, user authentication, fix bugs
    
    * send back proper data when authenticate user
    
    * fix form render issue
    
    * review functionality mvp completed
    
    * fix minor bugs
    
    * fix array bug

--- src/views/ShowMovieDetails/Reviews/Reviews.jsx
+++ src/views/ShowMovieDetails/Reviews/Reviews.jsx
@@ -19,7 +19,7 @@ export default function Reviews({ movieID }) {
 
   return (
     <ReviewsContainer>
-      {reviews
+      {reviews.length
         ? reviews.map(
             (item) =>
               movieID === parseInt(item.movieID) && (
