578c04fdfa29e6f4c9a578da8a6973f484730098
unknown <elam@nabi.nu>
2021-06-10 22:38:01 +0200

    small fixes before release

--- src/views/Kids/KidsStyled.js
+++ src/views/Kids/KidsStyled.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { MoviesContainer, HeaderMovies } from '../Movies/MoviesStyled';
 
-export const KidsContainer = styled(MoviesContainer)``
+export const KidsContainer = styled(MoviesContainer)`
+padding-top: 15em;
+`
 
 export const HeaderKids = styled(HeaderMovies)``
