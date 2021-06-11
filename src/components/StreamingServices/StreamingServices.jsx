b91ffdcdbf1acde20bd96a934a9825cec5ce37e9
Max Aakre <maxaakre@gmail.com>
2021-06-11 11:12:05 +0200

    hide component on startpage when logged in

--- src/components/StreamingServices/StreamingServices.jsx
+++ src/components/StreamingServices/StreamingServices.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 import {StreamingContainer,SteamingButton} from './StreamingServicesStyled';
 import Card from './ServiceCards';
@@ -19,8 +19,13 @@ export  const StreamingServices = () => {
         setShow(true);
         setSignup(true);  
     } 
+    const [loggedIn, setLoggedIn] = useState(false) 
+    useEffect(() => {
+        if (sessionStorage.getItem("__user__")) setLoggedIn(true);
+      }, []);
     return (
         <StreamingContainer>
+        {!loggedIn ?
             <section className="container">
                 <h3>All Streaming services in one place</h3>
                 <h2> Find something great to watch even faster</h2>
@@ -37,8 +42,10 @@ export  const StreamingServices = () => {
             <Modal setShow={setShow} show={show}>
                 <SignupForm setSignup={setSignup} signup={signup}/>    
             </Modal>
-            <SteamingButton onClick={openSignupForm}>Create a free account</SteamingButton> 
-            </section>
-        </StreamingContainer>
-    )
+           
+                <SteamingButton onClick={openSignupForm}>Create a free account</SteamingButton> 
+                </section>
+                :null}
+                </StreamingContainer>
+                )
 }
